f370cefb7becf039fe3e4c83ce6784ff348ce0e9
hosenA51 <hosenahmed2552@gmail.com>
2024-12-16 19:07:20 +0600

    implement JWT

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 require('dotenv').config()
 
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
+
+const logger = (req, res, next) =>{
+    console.log('inside the logger');
+    next();
+}
+
+const verifyToken = (req, res, next) => {
+    // console.log('inside verify token middleware', req.cookies);
+    const token = req?.cookies?.token;
+
+    if(!token){
+        return res.status(401).send({message: 'Unauthorized access'})
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+        if(err){
+            return res.status(401).send({message: 'Unauthorized access'})
+        }
+        req.user = decoded;
+        next();
+    })
+
+    
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nweo5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -31,12 +61,26 @@ async function run() {
         const jobsCollection = client.db('jobPortal').collection('jobs');
         const jobApplicationCollection = client.db('jobPortal').collection('job_applications');
 
+        // Auth related API's
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' });
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false, // for localhost
+                })
+                .send({ success: true });
+        })
+
+
         // jobs related API's
-        app.get('/jobs', async (req, res) => {
+        app.get('/jobs', logger, async (req, res) => {
+            console.log('now inside the api callback');
             const email = req.query.email;
             let query = {};
-            if(email){
-                query = {hr_email: email}
+            if (email) {
+                query = { hr_email: email }
             }
             const cursor = jobsCollection.find(query);
             const result = await cursor.toArray();
@@ -50,26 +94,31 @@ async function run() {
             res.send(result);
         })
 
-        app.post('/jobs', async(req, res)=> {
+        app.post('/jobs', async (req, res) => {
             const newJob = req.body;
             const result = await jobsCollection.insertOne(newJob);
             res.send(result);
         })
 
-        // job application apis
+        // job application api's
         // get all data, get one data, get some data [0, 1, many]
-        app.get('/job-application', async (req, res) => {
+        app.get('/job-application', verifyToken, async (req, res) => {
             const email = req.query.email;
             const query = { applicant_email: email }
+
+            if(req.user.email !== req.query.email){
+                return res.status(403).send({message: 'forbidden access'});
+            }
+
             const result = await jobApplicationCollection.find(query).toArray();
 
             // fokira way to aggregate data
-            for(const application of result){
+            for (const application of result) {
                 console.log("Full Application:", application);
                 console.log(application.job_id)
-                const query1 = {_id: new ObjectId(application.job_id)}
+                const query1 = { _id: new ObjectId(application.job_id) }
                 const job = await jobsCollection.findOne(query1);
-                if(job){
+                if (job) {
                     application.title = job.title;
                     application.location = job.location;
                     application.company = job.company;
@@ -82,9 +131,9 @@ async function run() {
 
         // app.get('/job-applications/:id') ==> get a specific job application by id
 
-        app.get('/job-applications/jobs/:job_id', async(req, res) => {
+        app.get('/job-applications/jobs/:job_id', async (req, res) => {
             const jobId = req.params.job_id;
-            const query = {job_id: jobId}
+            const query = { job_id: jobId }
             const result = await jobApplicationCollection.find(query).toArray();
             res.send(result);
         })
@@ -95,16 +144,16 @@ async function run() {
 
             // Not the best way (use aggregate)
             const id = application.job_id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const job = await jobsCollection.findOne(query);
             let newCount = 0;
-            if(job.applicationCount){
+            if (job.applicationCount) {
                 newCount = job.applicationCount + 1;
-            }else {
+            } else {
                 newCount = 1;
             }
             // now update the job info
-            const filter = {_id: new ObjectId(id)};
+            const filter = { _id: new ObjectId(id) };
             const updatedDoc = {
                 $set: {
                     applicationCount: newCount
@@ -115,10 +164,10 @@ async function run() {
             res.send(result);
         })
 
-        app.patch('/job-applications/:id', async(req, res)=>{
+        app.patch('/job-applications/:id', async (req, res) => {
             const id = req.params.id;
             const data = req.body;
-            const filter = {_id: new ObjectId(id)};
+            const filter = { _id: new ObjectId(id) };
             const updatedDoc = {
                 $set: {
                     status: data.status
